1fa93c7c95b6cbdf81e710be8d517714197d1688
Isaque123Barbosa <iba2@aluno.ifal.edu.br>
2025-05-27 10:49:23 -0300

    Inicio da realizacao da tela de autocuidado

---
+++ lib/pages/autocuidado.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Autocuidado extends StatefulWidget {
+  const Autocuidado({super.key});
+
+  @override
+  State<Autocuidado> createState() => _AutocuidadoState();
+}
+
+class _AutocuidadoState extends State<Autocuidado> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: ,
+          body: ,
+          bottomNavigationBar: ,
+        )
+    );
+  }
+
+  build
+}
