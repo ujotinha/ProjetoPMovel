529b26d53a774428f2a23c3076d122beb162de68
Emmy <embs5@aluno.ifal.edu.br>
2025-06-09 13:16:38 -0300

    Emmy versão 1

---
+++ lib/pages/autocuidado_medicamentos.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AutocuidadoMedicamentos extends StatefulWidget {
+  const AutocuidadoMedicamentos({super.key});
+
+  @override
+  State<AutocuidadoMedicamentos> createState() => _AutocuidadoMedicamentosState();
+}
+
+class _AutocuidadoMedicamentosState extends State<AutocuidadoMedicamentos> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: ,
+          body: ,
+          floatingActionButton: ,
+        )
+    );
+  }
+
+  buildAppBar(){
+    return AppBar(
+
+    )
+  }
+}
