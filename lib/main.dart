9cf0b132e147ae1830860ef9cea3656507f88fae
Hugo <hcas1@aluno.ifal.edu.br>
2025-06-10 11:41:31 -0300

    Tela de anotação finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:projetointheirskin/pages/anotacao.dart';
 import 'package:projetointheirskin/pages/autocuidado.dart';
 import 'package:projetointheirskin/pages/inicio.dart';
 
+
 void main() {
   runApp(MaterialApp(home:Anotacao()));
 }
--- lib/pages/anotacao.dart
+++ lib/pages/anotacao.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Anotacao extends StatefulWidget {
   const Anotacao({super.key});
 
@@ -21,7 +22,7 @@ class _AnotacaoState extends State<Anotacao> {
   buildAppBar(){
     return AppBar(
       backgroundColor: Color(0xFFe0d4bd),
-      title: Text('In Their Skin', style: TextStyle(color:Colors.brown),),
+      title: Image.asset("assets/logo.png", height: 50),
       shape: ContinuousRectangleBorder(
         borderRadius: BorderRadius.only(
           bottomLeft: Radius.circular(90.0),
@@ -43,23 +44,80 @@ buildbody(){
             borderRadius: BorderRadius.all(Radius.circular(16))
           ),
           height: 800,
-          child: Container(
-            child:Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.keyboard_arrow_left,
-                    color: Colors.orange,
-                  ),
-                  Spacer(),
-                  Icon(
-                    Icons.lock_open,
-                    color: Color(0xFF7e4f27),
-  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.keyboard_arrow_left,
+                        color: Colors.orange,
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.lock_open,
+                        color: Color(0xFF7e4f27),
+                      ),
+                      Icon(
+                        Icons.text_format,
+                        color: Color(0xFF7e4f27),
+                      ),
+                      Icon(
+                        Icons.title,
+                        color: Color(0xFF7e4f27),
+                      ),
+                      Icon(
+                        Icons.image,
+                        color: Color(0xFF7e4f27),
+                      ),
+
+                ],
+
+
+
+              ),
+
+
+                      ),
+
+              Container(
+                height: 570,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Título da nota", textAlign: TextAlign.justify, style: TextStyle(fontSize: 23, color: Color(0xFF834d30))),
+                    Text("Escreva seu registro aqui...", textAlign: TextAlign.justify, style: TextStyle(fontSize: 13, color: Color(0xFF834d30))),
+
+
+                  ],
+                ),
+              ),
+
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 115,
+                      height: 30,
+                      child: ElevatedButton(
+                        onPressed: () => {},
+                        child: Text("Salvar"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFa5591f),
+                          foregroundColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
 
-        ),
   ),
     );
 
