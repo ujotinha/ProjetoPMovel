d022be20f62ec44d95c1d8a72c9555a2bbc16716
Emmy <embs5@aluno.ifal.edu.br>
2025-08-19 16:04:41 -0300

    Continuando o desenvolvimento do banco de dados e da tela de medicamentos do autocuidado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projetointheirskin/pages/anotacao.dart';
+import 'package:projetointheirskin/pages/autocuidado_medicamento.dart';
 import 'package:projetointheirskin/pages/home_page.dart';
 import 'package:projetointheirskin/pages/inicio.dart';
 import 'package:projetointheirskin/pages/autocuidado_adc_medicamentos.dart';
@@ -7,6 +8,6 @@ import 'package:projetointheirskin/pages/personagem.dart';
 
 void main() {
 
-  runApp(MaterialApp(home:Homepage()));
+  runApp(MaterialApp(home:AutocuidadoMedicamentos()));
 
 }
