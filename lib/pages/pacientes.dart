7a553ceab0bf11bb85b7625a39a34f938168435b
Isa <ifs15@aluno.ifal.edu.br>
2025-08-21 19:47:36 -0300

    Tela Isa Widgets divididos

--- lib/pages/pacientes.dart
+++ lib/pages/pacientes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:projetointheirskin/widgets/CardBotaoCuidarPaciente.dart';
+import 'package:projetointheirskin/widgets/CardInformacoesPaciente.dart';
+import 'package:projetointheirskin/widgets/CardPlanoTratamento.dart';
 
 class Pacientes extends StatefulWidget {
   const Pacientes({super.key});
@@ -15,262 +18,22 @@ class _PacientesState extends State<Pacientes> {
       child: Container(
         padding: EdgeInsets.all(30),
         width: double.infinity,
-        height: 700,
+        height: double.infinity,
         decoration: BoxDecoration(
             color: Color(0xFFf0e6d4), borderRadius: BorderRadius.circular(20)),
         child: ListView(
           children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: SizedBox(
-                        width: 90,
-                        height: 90,
-                        child: CircleAvatar(
-                          child: ClipOval(
-                            child: Image(
-                                image: AssetImage("assets/fotoPessoa.png")),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      children: [
-                        Text("José Rodrigues Silva",
-                            style: TextStyle(color: Color(0xFF846446))),
-                        Container(
-                          height: 20,
-                          width: 175,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Color(0xFFdfceb4),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("Portador de Câncer de Pulmão",
-                                  style: TextStyle(
-                                      color: Color(0xFF7b4a28), fontSize: 10)),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            CardInformacoesPaciente(),
             SizedBox(
-              height: 30,
+              height: 25,
             ),
-            Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Color(0xFFdfceb4),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Column(
-                  spacing: 8,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.medical_information,
-                            color: Color(0xFF7b4a28), size: 20),
-                        Text("Informações do paciente",
-                            style: TextStyle(
-                                color: Color(0xFF7b4a28), fontSize: 12)),
-                      ],
-                    ),
-                    Container(
-                        padding: EdgeInsets.all(7),
-                        height: 28,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Nome completo:",
-                                style: TextStyle(
-                                    color: Color(0xFF7b4a28), fontSize: 10)),
-                          ],
-                        )),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                            padding: EdgeInsets.all(7),
-                            height: 28,
-                            width: 180,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Data de Nascimento:",
-                                    style: TextStyle(
-                                        color: Color(0xFF7b4a28),
-                                        fontSize: 10)),
-                              ],
-                            )),
-                        Container(
-                            padding: EdgeInsets.all(7),
-                            height: 28,
-                            width: 120,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("CPF:",
-                                    style: TextStyle(
-                                        color: Color(0xFF7b4a28),
-                                        fontSize: 10)),
-                              ],
-                            )),
-                      ],
-                    )
-                  ],
-                )),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Color(0xFFdfceb4),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Column(
-                  spacing: 8,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.monitor_heart,
-                            color: Color(0xFF7b4a28), size: 20),
-                        Text("Planos de Tratamento",
-                            style: TextStyle(
-                                color: Color(0xFF7b4a28), fontSize: 12)),
-                      ],
-                    ),
-                    Container(
-                        padding: EdgeInsets.all(7),
-                        height: 70,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Diagnóstico detalhado:",
-                                style: TextStyle(
-                                    color: Color(0xFF7b4a28), fontSize: 10)),
-                          ],
-                        )),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                            padding: EdgeInsets.all(7),
-                            height: 28,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text("Medicamentos",
-                                    style: TextStyle(
-                                        color: Color(0xFF7b4a28),
-                                        fontSize: 10)),
-                              ],
-                            )),
-                        Container(
-                            padding: EdgeInsets.all(7),
-                            height: 28,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text("Terapia",
-                                    style: TextStyle(
-                                        color: Color(0xFF7b4a28),
-                                        fontSize: 10)),
-                              ],
-                            )),
-                        Container(
-                            padding: EdgeInsets.all(7),
-                            height: 28,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text("Quimioterapia",
-                                    style: TextStyle(
-                                        color: Color(0xFF7b4a28),
-                                        fontSize: 10)),
-                              ],
-                            )),
-                      ],
-                    )
-                  ],
-                )),
+            CardPlanoTratamento(),
             SizedBox(
               height: 50,
             ),
             Center(
-              child:
-              SizedBox(
-                width: 200,
-                height: 40,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(10),
-                      backgroundColor: Color(0xFFca876d),
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Cuidar deste paciente",
-                            style: TextStyle(color: Color(0xFFffe3d7))),
-                      ],
-                    )),
-              ),
+              child: CardBotaoCuidarPaciente(),
             ),
-
           ],
         ),
       ),
---
+++ lib/widgets/CardInformacoesPaciente.dart
@@ -0,0 +1,160 @@
+import 'package:projetointheirskin/pages/pacientes.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CardInformacoesPaciente extends StatefulWidget {
+  //Propriedade propriedade;
+
+  CardInformacoesPaciente({
+    //required this.propriedade,
+    super.key,
+  });
+
+  @override
+  State<CardInformacoesPaciente> createState() =>
+      _CardInformacoesPacienteState();
+}
+
+class _CardInformacoesPacienteState extends State<CardInformacoesPaciente> {
+  // String get urlImage => widget.urlImage;
+  //Propriedade get propriedade => widget.propriedade;
+
+  // String getUrlImage {
+  //   return widget.urlImage;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    child: SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: CircleAvatar(
+                        child: ClipOval(
+                          child:
+                              Image(image: AssetImage("assets/fotoPessoa.png")),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    children: [
+                      Text("José Rodrigues Silva",
+                          style: TextStyle(color: Color(0xFF846446))),
+                      Container(
+                        height: 20,
+                        width: 175,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xFFdfceb4),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Portador de Câncer de Pulmão",
+                                style: TextStyle(
+                                    color: Color(0xFF7b4a28), fontSize: 10)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color(0xFFdfceb4),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                spacing: 8,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.medical_information,
+                          color: Color(0xFF7b4a28), size: 20),
+                      Text("Informações do paciente",
+                          style: TextStyle(
+                              color: Color(0xFF7b4a28), fontSize: 12)),
+                    ],
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(7),
+                      height: 28,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Nome completo:",
+                              style: TextStyle(
+                                  color: Color(0xFF7b4a28), fontSize: 10)),
+                        ],
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                          padding: EdgeInsets.all(7),
+                          height: 28,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Data de Nascimento:",
+                                  style: TextStyle(
+                                      color: Color(0xFF7b4a28), fontSize: 10)),
+                            ],
+                          )),
+                      Container(
+                          padding: EdgeInsets.all(7),
+                          height: 28,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("CPF:",
+                                  style: TextStyle(
+                                      color: Color(0xFF7b4a28), fontSize: 10)),
+                            ],
+                          )),
+                    ],
+                  )
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
