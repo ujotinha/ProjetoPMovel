4669e0137508a7532dfc8ccf2a72c03bbd1068ec
mavi <mvsp5@aluno.ifal.edu.br>
2025-10-23 14:48:48 -0300

    api real finalizado

--- lib/pages/consultas.dart
+++ lib/pages/consultas.dart
@@ -14,7 +14,6 @@ class consultas extends StatefulWidget {
 }
 
 class _consultasState extends State<consultas> {
-
   //List listaconsultas = [];
   late Future<List<Consulta>> futureListaconsultas;
   late Future<Versiculo> versiculoAleatorio;
@@ -42,79 +41,88 @@ class _consultasState extends State<consultas> {
     );
   }
 
-    buildBody() {
-      return Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Container(
+  buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            color: Color(0xFFf0e6d4), borderRadius: BorderRadius.circular(20)),
+        child: Padding(
           padding: EdgeInsets.all(8.0),
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-              color: Color(0xFFf0e6d4),
-              borderRadius: BorderRadius.circular(20)),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    IconButton(onPressed: () {},
-                      icon: Icon(Icons.arrow_back_ios),
-                      color: Color(0xFFc77b44),
-                      iconSize: 20,),
-                    Text("Consultas",
-                        style: TextStyle(
-                            fontSize: 20, color: Color(0xFFc77b44))),
-                  ],
-                ),
-
-                SizedBox(height: 10),
-                Expanded(
-                  child: FutureBuilder<List<Consulta>>(
-                    future: futureListaconsultas,
-                    builder: (context, snapshot){
-                    if (snapshot.hasData){
-                      List<Consulta> lista = snapshot.requireData;
-                      return buildListView(lista);
-                      }
-                    return Center(child: CircularProgressIndicator());
-                    }
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back_ios),
+                    color: Color(0xFFc77b44),
+                    iconSize: 20,
                   ),
-
-                ),
-              ],
-            ),
+                  Text("Consultas",
+                      style: TextStyle(fontSize: 20, color: Color(0xFFc77b44))),
+                ],
+              ),
+              SizedBox(height: 10),
+              FutureBuilder<Versiculo>(
+                  future: versiculoAleatorio,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      Versiculo versiculo = snapshot.requireData;
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            children: [
+                              Text("${versiculo.livro} ${versiculo.capitulo}:${versiculo.numero_versiculo} ${versiculo.texto}")
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                    return Center(child: CircularProgressIndicator());
+                  }),
+              SizedBox(height: 10),
+              Expanded(
+                child: FutureBuilder<List<Consulta>>(
+                    future: futureListaconsultas,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Consulta> lista = snapshot.requireData;
+                        return buildListView(lista);
+                      }
+                      return Center(child: CircularProgressIndicator());
+                    }),
+              ),
+            ],
           ),
         ),
-      );
-    }
-  }
-
-  buildListView(List<Consulta> listconsultas){
-    return ListView.builder(
-      itemCount: listconsultas.length,
-      itemBuilder: (context, i){
-        return CardConsulta(
-          consulta: listconsultas[i]
-        );
-      },
-    );
-  }
-
-  buildFloatingActionButton() {
-    return FloatingActionButton(
-      backgroundColor: Color(0xFFa5591f),
-      onPressed: () {},
-      shape: CircleBorder(),
-      child: Icon(
-        Icons.add,
-        color: Colors.white,
       ),
     );
   }
+}
 
+buildListView(List<Consulta> listconsultas) {
+  return ListView.builder(
+    itemCount: listconsultas.length,
+    itemBuilder: (context, i) {
+      return CardConsulta(consulta: listconsultas[i]);
+    },
+  );
+}
 
-
-
-
+buildFloatingActionButton() {
+  return FloatingActionButton(
+    backgroundColor: Color(0xFFa5591f),
+    onPressed: () {},
+    shape: CircleBorder(),
+    child: Icon(
+      Icons.add,
+      color: Colors.white,
+    ),
+  );
+}
